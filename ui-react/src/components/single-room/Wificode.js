405d29e5b8b0669389b34eb6821b54244c4da45c
Sölvi Sig <solvisig@gmail.com>
2020-01-15 14:58:16 +0000

    Guest wifi to sidebar

---
+++ ui-react/src/components/single-room/Wificode.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import * as config from "../../config/singleRoom.config.js";
+import wifiqr from "../../imgs/wifiqrcode.png";
+
+class Wifi extends Component {
+  render() {
+    return (
+      <div id="single-room__wifi">
+        <div id="single-room__wifi-title">
+          <h2>{config.wifiTitle}</h2>
+        </div>
+        <img id="single-room__wifi-image" src={wifiqr} alt="Wifi Code" />
+        <div id="single-room__wifi-text">
+          <p>Wifi: OpinKerfi-Guest</p>
+          <p>Password: opinkerfi2019</p>
+        </div>
+      </div>
+    );
+  }
+}
+export default Wifi;
